e50817447e33fb50e6e002d97b660d722024bbd8
Atharva <kharabeah@rknec.edu>
2021-09-14 11:49:09 +0530

    Updated Events cards and added basic dialog boxes

--- src/components/Events/OurEvents.js
+++ src/components/Events/OurEvents.js
@@ -18,18 +18,24 @@ function OurEvents(){
     const classes=useStyles();
     return(
         <div>
-        <Box m={8}>
-            <Grid container justifyContent='center' spacing={3}>
-                <Grid item container xs={12} sm={12} justifyContent='center'>
+        <Box m={10}>
+            <Grid container justifyContent='center' spacing={2}>
+                <Grid item container xs={12} justifyContent='center'>
                     <Typography className={classes.title}>
                     Our Events
                     </Typography>
                 </Grid>
+                <Grid item xs={0} sm={1}>
+                </Grid>
+                <Grid container item xs={12} sm={10} justifyContent='center' spacing={7} >
                 {Arr.map((e) => (
-                    <Grid item container xs={12} sm={6} justifyContent='center'>
+                    <Grid item container xs={12} sm={4} justifyContent='center'>
                         <EventCard event={e} />
                     </Grid>
                 ))} 
+                </Grid>
+                <Grid item sm={1} xs={0}>
+                </Grid>
             </Grid>
         </Box>
         </div>
